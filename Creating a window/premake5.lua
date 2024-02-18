dc53beb214150540e0a0ecc7a9563ceb39249548
Crosslywere <ogboruj@outlook.com>
2024-02-18 18:47:32 +0100

    Updated include directories

--- Creating a window/premake5.lua
+++ Creating a window/premake5.lua
@@ -4,5 +4,4 @@ language "C++"
 files { "src/**.cpp", "src/**.h" }
 targetdir "%{prj.location}/bin/%{cfg.buildcfg}"
 objdir "%{prj.location}/obj/%{cfg.buildcfg}"
-includedirs "%{wks.location}/deps/glad/include"
 links { "glad", "glfw3" }
